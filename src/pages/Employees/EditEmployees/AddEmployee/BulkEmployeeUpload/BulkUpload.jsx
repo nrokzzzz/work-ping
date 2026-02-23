793e31553390802f2a0667be18a2f56f74f1a117
Aravelli Vishnu Priya <23A91A05E1@aec.edu.in>
2026-02-23 13:37:37 +0530

    Updated BulkUploads[priya]

--- src/pages/Employees/EditEmployees/AddEmployee/BulkEmployeeUpload/BulkUpload.jsx
+++ src/pages/Employees/EditEmployees/AddEmployee/BulkEmployeeUpload/BulkUpload.jsx
@@ -12,15 +12,19 @@ import PageMetaData from '@/components/PageTitle'
 import { useNavigate } from 'react-router-dom'
 import IconifyIcon from '@/components/wrappers/IconifyIcon'
 import axiosClient from '@/helpers/httpClient'
+import TwoFactorAuthentication from "@/pages/TwoFactorAuthentication/TwoFactorAuthentication";
 
 const BulkUpload = () => {
   const navigate = useNavigate()
 
-  const [file, setFile] = useState(null) // ✅ single file
+  const [file, setFile] = useState(null) 
   const [loading, setLoading] = useState(false)
   const [uploadProgress, setUploadProgress] = useState(0)
   const [allTasks, setAllTasks] = useState([])
   const [showTable, setShowTable] = useState(false)
+   const [show2FA, setShow2FA] = useState(false);
+
+  
 
   const fileInputRef = useRef(null)
 
@@ -31,7 +35,7 @@ const BulkUpload = () => {
   }
 
   const handleFileChange = (e) => {
-    const selectedFile = e.target.files[0] // ✅ only first file
+    const selectedFile = e.target.files[0] 
     if (selectedFile) {
       setFile(selectedFile)
     }
@@ -49,7 +53,7 @@ const BulkUpload = () => {
       alert('Please select a file')
       return
     }
-
+    setShow2FA(true);
     const formData = new FormData()
     formData.append('file', file)
 
@@ -165,6 +169,16 @@ const BulkUpload = () => {
               >
                 {loading ? 'Uploading...' : 'Upload'}
               </Button>
+               {show2FA && (
+                    <TwoFactorAuthentication
+                      onSuccess={() => {
+                        setShow2FA(false);
+                      }}
+                      onCancel={() => {
+                        setShow2FA(false);
+                      }}
+                    />
+                  )}
             </div>
 
             {/* Progress Bar */}
