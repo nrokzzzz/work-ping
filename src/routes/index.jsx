639feee41a2556e1d5aadf6a30f4ba0c64b7d66a
Chavali J S R Vinayaka Gopala Krishna <147472515+VinayakaGopalaKrishnA123@users.noreply.github.com>
2026-03-11 14:21:56 +0000

    index route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -528,6 +528,7 @@ const AddTeams = lazy(()=>import('@/pages/Teams(Department)/EditTeams/AddTeams/A
 const UpdateTeams = lazy(()=>import('@/pages/Teams(Department)/EditTeams/UpdateTeams/Update/UpdateTeams'));
 const UpdateTeamsView = lazy(()=>import('@/pages/Teams(Department)/EditTeams/UpdateTeams/Update/UpdateTeamsView'));
 const ViewTeams = lazy(()=>import('@/pages/Teams(Department)/ViewTeams/ViewTeams'))
+const TeamMembersView = lazy(()=>import('@/pages/Teams(Department)/TeamMembers/TeamMembersView'))
       //organization routes
 const OrganizationViews = lazy(()=>import('@/pages/Organization/ViewOrganization/View'))
 const OrganizationUpdateDetails= lazy(()=>import('@/pages/Organization/EditOrganization/UpdateOrganization/OrganizationDetails'));
@@ -622,7 +623,11 @@ const teamsRoutes=[
   path:'/teams/update-teams-view',
   element:<UpdateTeamsView/>
 }
-,{},{
+,{
+  name:'TeamMembersView',
+  path:'/teams/team-members/team-members-view/:teamId',
+  element:<TeamMembersView/>
+},{
   name:'TeamsView',
   path:'/teams/view-teams/',
   element:<ViewTeams/>
